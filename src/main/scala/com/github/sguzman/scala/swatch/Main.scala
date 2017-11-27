653c5a5b3df73cca72e7bc403e9f9fea79bb9083
Salvador Guzman <guzmansalv@gmail.com>
2017-11-27 07:21:38 -0800

    retrieve cartoon list

--- src/main/scala/com/github/sguzman/scala/swatch/Main.scala
+++ src/main/scala/com/github/sguzman/scala/swatch/Main.scala
@@ -1,13 +1,19 @@
 package com.github.sguzman.scala.swatch
 
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.elementList
 
 import scalaj.http.Http
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val shows = cartoons
+
+    println(shows)
+  }
+
+  def cartoons = {
     val url = "https://www.watchcartoononline.io/cartoon-list"
     val request = Http(url)
     val response = request.asString
@@ -16,6 +22,7 @@ object Main {
     val doc = browser.parseString(response.body)
     val shows = doc >> elementList("#ddmcc_container > div > ul > ul > li > a")
 
-    println(shows)
+    val tuple = shows.map(e => (e.text, e.attr("href")))
+    tuple
   }
 }
